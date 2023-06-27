4714b7fcdcc021c0b2683e7b8e39cd580b275e6b
Coding in Flow <info@codinginflow.com>
2023-06-27 11:39:31 +0200

    End of part 6

---
+++ src/app/loading.tsx
@@ -0,0 +1,3 @@
+export default function LoadingPage() {
+  return <span className="loading loading-dots loading-lg m-auto block" />;
+}
---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,57 @@
+import PriceTag from "@/components/PriceTag";
+import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { cache } from "react";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Flowmazon",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
+
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+      </div>
+    </div>
+  );
+}
