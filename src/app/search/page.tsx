63bfaf8d3a08e89bbfe3c4b640f361bb9688c81b
Coding in Flow <info@codinginflow.com>
2023-06-29 12:49:04 +0200

    End of part 13

---
+++ src/app/search/page.tsx
@@ -0,0 +1,41 @@
+import ProductCard from "@/components/ProductCard";
+import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
+
+interface SearchPageProps {
+  searchParams: { query: string };
+}
+
+export function generateMetadata({
+  searchParams: { query },
+}: SearchPageProps): Metadata {
+  return {
+    title: `Search: ${query} - Flowmazon`,
+  };
+}
+
+export default async function SearchPage({
+  searchParams: { query },
+}: SearchPageProps) {
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: query, mode: "insensitive" } },
+        { description: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    orderBy: { id: "desc" },
+  });
+
+  if (products.length === 0) {
+    return <div className="text-center">No products found</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
+  );
+}
